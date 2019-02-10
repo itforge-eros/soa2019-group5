169f75aa0cfbfc8c3e96d8db7a19ee6565b12d56
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2019-02-10 17:00:28 +0700

    Add HomePage component

--- lectio-pwa/src/App.tsx
+++ lectio-pwa/src/App.tsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { Route, NavLink, HashRouter } from 'react-router-dom';
+import HomePage from './containers/HomePage';
 import './App.sass';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+	render() {
+		return (
+			<HashRouter>
+				<div className="App">
+					<Route path="/" component={HomePage} />
+				</div>
+			</HashRouter>
+		);
+	}
 }
 
 export default App;
---
+++ lectio-pwa/src/containers/HomePage.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+  render() {
+		return (
+			<h1>This is Home</h1>
+		)
+  }
+}
+
+export default HomePage;
