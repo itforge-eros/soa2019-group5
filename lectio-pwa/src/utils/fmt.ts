64b4b281f9915acbfa6cf55c31ba7044717b1b8c
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2019-03-03 09:14:31 +0700

    Add JSDoc to fmt

--- lectio-pwa/src/utils/fmt.ts
+++ lectio-pwa/src/utils/fmt.ts
@@ -1,3 +1,7 @@
+/**
+ * Formats an array into a comma-separated list
+ * @param list - An array of items
+ */
 export const listWithCommas = (list: Array<string>) : string => {
 	let result = '';
 	list.forEach((item, index) => result += item + (index < list.length - 1 ? ', ' : ''));
