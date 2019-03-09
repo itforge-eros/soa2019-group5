bfad24a2657a2a13c015dabdcd1491832fb5e896
Nathan Yiangsupapaanonntr <zartre.y@gmail.com>
2019-03-09 22:04:44 +0700

    Add top bar

--- lectio-pwa/src/containers/MemoPage.tsx
+++ lectio-pwa/src/containers/MemoPage.tsx
@@ -1,13 +1,31 @@
 import React, { Component, Fragment } from 'react';
+import { AppBar, IconButton, Toolbar } from '@material-ui/core';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+import { withRouter } from 'react-router-dom';
 
-class MemoPage extends Component {
+class MemoPage extends Component<any, any> {
+	constructor(props: any) {
+		super(props);
+	}
+
+	handleBackBtn() {
+		setTimeout(() => this.props.history.push('/'), 200);
+	}
+
 	render() {
 		return (
 			<Fragment>
-				<h1>LEL</h1>
+				<AppBar position="fixed" color="default">
+					<Toolbar>
+						<IconButton onClick={() => this.handleBackBtn()}>
+							<ArrowBack />
+						</IconButton>
+					</Toolbar>
+				</AppBar>
+				<p>lorem ipsum</p>
 			</Fragment>
 		);
 	}
 }
 
-export default MemoPage;
+export default withRouter(MemoPage);
