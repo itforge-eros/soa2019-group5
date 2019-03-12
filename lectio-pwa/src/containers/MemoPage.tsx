456ed15d2e0eeca01869aefae4a655a44dfe702c
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2019-03-12 21:15:44 +0700

    Add Memo page layout (#8)
    * Add top bar
    
    * Ignore IDEA files
    
    * Fix ignoring IDEA
    
    * Add sections to Memo page
    
    * Add summary icon to app bar
    
    * Add delete btn to app bar
    
    * Add memo title
    
    * Make toolbar bg white
    
    * Add tag chips

--- lectio-pwa/src/containers/MemoPage.tsx
+++ lectio-pwa/src/containers/MemoPage.tsx
@@ -1,13 +1,61 @@
 import React, { Component, Fragment } from 'react';
+import { AppBar, Button, Chip, IconButton, Toolbar, Typography } from '@material-ui/core';
+import { Add as AddIcon, ArrowBack, Delete, ScatterPlot } from '@material-ui/icons';
+import { withRouter } from 'react-router-dom';
+import styles from './MemoPage.module.sass';
 
-class MemoPage extends Component {
+const inlineStyles = {
+	toolbar: {
+		paddingLeft: '8px',
+		paddingRight: '8px',
+		backgroundColor: '#fff'
+	}
+}
+
+class MemoPage extends Component<any, any> {
+	constructor(props: any) {
+		super(props);
+	}
+
+	handleBackBtn() {
+		setTimeout(() => this.props.history.push('/'), 200);
+	}
+
 	render() {
 		return (
 			<Fragment>
-				<h1>LEL</h1>
+				<AppBar position="fixed" color="default" elevation={0}>
+					<Toolbar style={inlineStyles.toolbar}>
+						<IconButton onClick={() => this.handleBackBtn()}>
+							<ArrowBack />
+						</IconButton>
+						<div className={styles.grow}></div>
+						<IconButton>
+							<Delete />
+						</IconButton>
+						<IconButton>
+							<ScatterPlot />
+						</IconButton>
+					</Toolbar>
+				</AppBar>
+				<div className={styles.contentArea}>
+					<div className={styles.textArea}>
+						<Typography variant="h6">
+							Memo name here
+						</Typography>
+						<p>lorem ipsum</p>
+						<div className={styles.chipWrap}>
+							<Chip label="Demo tag" className={styles.chip} />
+							<Button>
+								<AddIcon fontSize="small" />
+							</Button>
+						</div>
+					</div>
+					<div className={styles.audioArea}></div>
+				</div>
 			</Fragment>
 		);
 	}
 }
 
-export default MemoPage;
+export default withRouter(MemoPage);
