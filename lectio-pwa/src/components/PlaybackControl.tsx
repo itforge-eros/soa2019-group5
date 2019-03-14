01ea8aae1858da491930ef8244fd1d930c1403f9
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2019-03-14 17:11:16 +0700

    Change FAB to blue

--- lectio-pwa/src/components/PlaybackControl.tsx
+++ lectio-pwa/src/components/PlaybackControl.tsx
@@ -9,7 +9,9 @@ const inlineStyles = {
         top: -30,
         left: 0,
         right: 0,
-        margin: '0 auto'
+        margin: '0 auto',
+        backgroundColor: '#0062F5',
+        color: '#fff'
     },
 };
 
@@ -17,7 +19,7 @@ class PlaybackControl extends Component {
     render() {
         return(
             <div className={styles.audioArea}>
-                <Fab color="primary" aria-label="Add" style={inlineStyles.fab}>
+                <Fab aria-label="Add" style={inlineStyles.fab}>
                     <PlayArrow />
                 </Fab>
             </div>
