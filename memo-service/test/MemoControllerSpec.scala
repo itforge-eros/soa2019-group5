5fe5bea78c93df8b25029c5056109ff510cfd2dc
Weeruhputt Supsohmboon <59070162@it.kmitl.ac.th>
2019-03-17 20:52:21 +0700

    WIP: Testing PUT endpoint

--- memo-service/test/MemoControllerSpec.scala
+++ memo-service/test/MemoControllerSpec.scala
@@ -1,5 +1,6 @@
 import java.util.UUID
 
+import akka.stream.Materializer
 import cats.effect.IO
 import models.{Memo, MemoForm}
 import org.scalatest.mockito.MockitoSugar
@@ -10,28 +11,44 @@ import play.api.test.Helpers._
 import repositories.MemoRepository
 import v1.memo.MemoController
 import org.mockito.Mockito._
-import play.api.libs.json.Json
+import play.api.libs.json.{Format, Json}
+import play.api.mvc.ControllerComponents
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class MemoControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting with MockitoSugar {
+class MemoControllerSpec extends PlaySpec
+  with GuiceOneAppPerTest
+  with Injecting
+  with MockitoSugar {
 
-  "MemoController" should {
+
+  "v1 MemoController" should {
+
+    implicit lazy val materializer: Materializer = app.materializer
+    implicit val memoFormFormat: Format[MemoForm] = Json.format[MemoForm]
+    implicit lazy val components: ControllerComponents = inject[ControllerComponents]
+
     "return 200 if success" in {
       val id = UUID.fromString("40d9894d-f25c-4594-bc6d-1aaad973f55d")
 
-      val controller = new MemoController(stubControllerComponents(), mockMemoRepository)
+      val controller = new MemoController(components, mockMemoRepository)
       val response = controller.get(id).apply(FakeRequest())
+
       status(response) mustBe OK
     }
 
     "return 200 if updated success" in {
       val id = UUID.fromString("40d9894d-f25c-4594-bc6d-1aaad973f55d")
-      val memoForm = MemoForm("Hellw, World!", "Hello, it's me!", "Hi", Set("simple", "hello"))
+      val memoForm = MemoForm("Hello, World!", "Hello, it's me!", "Hi", Set("simple", "hello"))
+      val existedMemo = Memo(id, "Hello, Thailand", "Hi, I'am Thai!", "Introduce", Set("introducing"))
 
       val mockMemoRepository = mock[MemoRepository]
       when(mockMemoRepository.updateMemo(id, memoForm)) thenReturn IO.pure(true)
-
+      val controller = new MemoController(components, mockMemoRepository)
+      val request = FakeRequest(PUT, s"/")
+        .withJsonBody(Json.toJson(memoForm))
+      val response = controller.put(id).apply(request)
+      status(response) mustBe OK
     }
   }
 
