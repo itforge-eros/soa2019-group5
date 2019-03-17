5c2e06bed4084d90ac13a040b4741cb1b5019ad5
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2019-03-17 16:51:55 +0700

    Add recording toggle

--- lectio-pwa/src/components/RecordControl.tsx
+++ lectio-pwa/src/components/RecordControl.tsx
@@ -3,6 +3,10 @@ import { Fab } from "@material-ui/core";
 import { FiberManualRecord, Pause } from "@material-ui/icons";
 import styles from './RecordControl.module.sass';
 
+interface State {
+	recording: boolean
+}
+
 const inlineStyles = {
 	Fab: {
 		color: '#E23939',
@@ -10,19 +14,24 @@ const inlineStyles = {
 	}
 };
 
-class RecordControl extends Component<any, any> {
+class RecordControl extends Component<any, State> {
 	constructor(props: any) {
 		super(props);
 		this.state = {
 			recording: true
 		}
+		this.handleFabClick = this.handleFabClick.bind(this);
+	}
+
+	private handleFabClick() {
+		this.setState(state => ({ recording: !state.recording }));
 	}
 
 	render() {
 		return(
-			<div className={styles.audioArea}>
+			<div className={`${styles.audioArea} ${this.state.recording ? styles.recording : ''}`}>
 				<p>Recording 02.39</p>
-				<Fab aria-label="Add" style={inlineStyles.Fab}>
+				<Fab aria-label="Add" style={inlineStyles.Fab} onClick={this.handleFabClick}>
 					{this.state.recording ? <Pause /> : <FiberManualRecord />}
 				</Fab>
 			</div>
