9dd5a59983286af9d6aec0548fac97c00945e2f2
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2019-03-26 09:39:16 +0700

    Frontend update March 20, 2019 (#19)
    * Add back action to Tag page
    
    * Add a search bar
    
    * Add tag list
    
    * Add search functionality
    
    * Show 'create a tag' when no matching query found
    
    * Add Record page
    
    * Add recording toggle
    
    * Disable text select in record control
    
    * Remove trailing slash
    
    * Toggle recording status text
    
    * Initialise recorder
    
    * Start recording when entering Record page
    
    * Add resume/pause action to FAB
    
    * Stop recording when leaving Record page
    
    * Show an error when recorder initialisation fails
    
    * Remove clear statement
    
    * Add finishRecording method

--- lectio-pwa/src/components/RecordControl.tsx
+++ lectio-pwa/src/components/RecordControl.tsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Fab } from "@material-ui/core";
-import { FiberManualRecord, Pause } from "@material-ui/icons";
+import { FiberManualRecord, Pause } from '@material-ui/icons';
+import Recorder from '../vendor/recorder';
 import styles from './RecordControl.module.sass';
 
 interface State {
-	recording: boolean
+	recording: boolean,
+	supportsRecording: boolean,
+	hasError: boolean
 }
 
 const inlineStyles = {
@@ -15,25 +18,74 @@ const inlineStyles = {
 };
 
 class RecordControl extends Component<any, State> {
+	rec: any;
+
 	constructor(props: any) {
 		super(props);
 		this.state = {
-			recording: true
-		}
+			recording: false,
+			supportsRecording: false,
+			hasError: false
+		};
 		this.handleFabClick = this.handleFabClick.bind(this);
+		this.handleSaveClick = this.handleSaveClick.bind(this);
 	}
 
-	private handleFabClick() {
+	componentDidMount() {
+		navigator.mediaDevices.getUserMedia({audio: true, video: false})
+			.then((stream) => {
+				// Set up recorder
+				console.log('Initiating recorder');
+				const audioContext: AudioContext = new AudioContext();
+				const input: MediaStreamAudioSourceNode = audioContext.createMediaStreamSource(stream);
+				this.rec = new Recorder(input);
+				this.setState({ supportsRecording: true });
+				console.log('Initiated recorder');
+
+				// Start recording
+				this.rec.record();
+				this.setState({ recording: true });
+			})
+			.catch((error) => {
+				console.log('Error initiating recorder');
+				this.setState({ supportsRecording: false, hasError: true });
+			});
+	}
+
+	componentWillUnmount() {
+		// TODO: Stop only if it's recording
+		if (this.rec) {
+			console.log('Stopping recording');
+			this.rec.stop();
+		}
+	}
+
+	private handleFabClick(): void {
+		if (this.state.recording) this.rec.stop(); // pause
+		else this.rec.record(); // resume
 		this.setState(state => ({ recording: !state.recording }));
+	}
+
+	/* public handleSaveClick(): void {
+		this.rec.stop();
+		this.rec.exportWav(this.finishRecording);
+	} */
+
+	private finishRecording(blob: Blob): void {
+		const url = URL.createObjectURL(blob);
+		console.log(url);
 	}
 
 	render() {
 		return(
 			<div className={`${styles.audioArea} ${this.state.recording ? styles.recording : ''}`}>
-				<p>{this.state.recording ? 'Recording' : 'Paused'} 02.39</p>
-				<Fab aria-label="Add" style={inlineStyles.Fab} onClick={this.handleFabClick}>
-					{this.state.recording ? <Pause /> : <FiberManualRecord />}
-				</Fab>
+				{ this.state.hasError && <p>Cannot set up recording</p> }
+				{ this.state.supportsRecording && <p>{ this.state.recording ? 'Recording' : 'Paused' } 02.39</p> }
+				{ this.state.supportsRecording &&
+					<Fab aria-label="Add" style={inlineStyles.Fab} onClick={this.handleFabClick}>
+						{ this.state.recording ? <Pause /> : <FiberManualRecord /> }
+					</Fab>
+				}
 			</div>
 		)
 	}
