5dc458d0df246e7b424bd3d9f1cba715338ab789
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2019-04-03 09:50:19 +0700

    Use global constant in idb

--- lectio-pwa/src/utils/idb.ts
+++ lectio-pwa/src/utils/idb.ts
@@ -1,9 +1,11 @@
+import {DB_NAME} from '../constants';
+
 export const openConnection = new Promise((resolve, reject) => {
-	const request = indexedDB.open('lectio-pwa');
+	const request = indexedDB.open(DB_NAME);
 	request.onsuccess = (event) => {
 		resolve();
 	};
 	request.onerror = (event) => {
-		reject();
+		reject(event);
 	};
 });
