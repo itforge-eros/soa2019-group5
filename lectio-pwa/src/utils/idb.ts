f3ea5b817107ea8524eaef0225645658f2cb2c0b
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2019-04-03 16:29:17 +0700

    Create a database store

--- lectio-pwa/src/utils/idb.ts
+++ lectio-pwa/src/utils/idb.ts
@@ -1,11 +1,22 @@
 import {DB_NAME} from '../constants';
 
 export const openConnection = new Promise((resolve, reject) => {
-	const request = indexedDB.open(DB_NAME);
-	request.onsuccess = (event) => {
-		resolve();
-	};
+	const request = indexedDB.open(DB_NAME, 2);
 	request.onerror = (event) => {
 		reject(event);
+	};
+	request.onupgradeneeded = (event) => {
+		console.log('db onupgradeneeded');
+		// @ts-ignore
+		const db = event.target.result;
+		const objectStore = db.createObjectStore('memo', { keyPath: 'id' });
+		objectStore.createIndex('name', 'name', { unique: false });
+		objectStore.createIndex('content', 'content', { unique: false });
+		objectStore.createIndex('audio', 'audio', { unique: false });
+		objectStore.createIndex('tags', 'tags', { unique: false });
+	};
+	request.onsuccess = (event) => {
+		console.log('db onsuccess');
+		resolve();
 	};
 });
