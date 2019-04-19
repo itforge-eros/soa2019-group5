aa677d0982b6d5ecb8e7c33321b53edb8d49ca46
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2019-04-19 10:44:23 +0700

    Add wavesurfer dependency

--- lectio-pwa/src/components/PlaybackControl.tsx
+++ lectio-pwa/src/components/PlaybackControl.tsx
@@ -4,30 +4,30 @@ import { PlayArrow } from "@material-ui/icons";
 import styles from './PlaybackControl.module.sass';
 
 const inlineStyles = {
-    fab: {
-        position: 'absolute' as 'absolute',
-        top: -30,
-        left: 0,
-        right: 0,
-        margin: '0 auto',
-        backgroundColor: '#0062F5',
-        color: '#fff'
-    },
+	fab: {
+		position: 'absolute' as 'absolute',
+		top: -30,
+		left: 0,
+		right: 0,
+		margin: '0 auto',
+		backgroundColor: '#0062F5',
+		color: '#fff'
+	},
 };
 
 /**
  * An audio playback control
  */
 class PlaybackControl extends Component {
-    render() {
-        return(
-            <div className={styles.audioArea}>
-                <Fab aria-label="Add" style={inlineStyles.fab}>
-                    <PlayArrow />
-                </Fab>
-            </div>
-        )
-    }
+	render() {
+		return(
+			<div className={styles.audioArea}>
+				<Fab aria-label="Add" style={inlineStyles.fab}>
+					<PlayArrow />
+				</Fab>
+			</div>
+		)
+	}
 }
 
 export default PlaybackControl;
