64b810303f376b2effd43f956d751213c5150639
kavinvin <kavinvin.vin@gmail.com>
2019-05-03 09:38:02 +0700

    Fix search not found due to unauthenticated user

--- search-service/src/main/scala/io/itforge/lectio/search/Server.scala
+++ search-service/src/main/scala/io/itforge/lectio/search/Server.scala
@@ -3,14 +3,14 @@ package io.itforge.lectio.search
 import cats.effect._
 import cats.implicits._
 import com.sksamuel.elastic4s.http.{ElasticClient, ElasticProperties}
+import io.circe.config.parser
+import io.circe.generic.auto._
+import io.itforge.lectio.search.auth.AuthService
+import io.itforge.lectio.search.config.SearchServiceConfig
 import io.itforge.lectio.search.memo.{MemoEndpoints, MemoRepositoryInterpreter, MemoService}
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.{Router, Server => Http4sServer}
 import org.http4s.syntax.kleisli._
-import io.circe.config.parser
-import io.circe.generic.auto._
-import io.itforge.lectio.search.auth.{AnonymousAuthService, AuthService}
-import io.itforge.lectio.search.config.SearchServiceConfig
 
 object Server extends IOApp {
 
@@ -20,7 +20,7 @@ object Server extends IOApp {
       client = ElasticClient(ElasticProperties(conf.elastic.uri))
       memoRepo = MemoRepositoryInterpreter[F](client, conf.elastic.memosIndex)
       memoService = MemoService[F](memoRepo)
-      authService = AnonymousAuthService[F]
+      authService = AuthService[F](conf.auth.publicKey)
       services = MemoEndpoints.endpoints[F](memoService, authService.middleware)
       httpApp = Router("/" -> services).orNotFound
       server <- BlazeServerBuilder[F]
