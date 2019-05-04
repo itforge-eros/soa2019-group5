6a73f7532307e1b98f7e82c28efa6cd52f54cdfc
kavinvin <kavinvin.vin@gmail.com>
2019-05-04 17:55:53 +0700

    Create memo with data #53

--- memo-service/app/controllers/api/memo.js
+++ memo-service/app/controllers/api/memo.js
@@ -4,6 +4,13 @@ const Memo = mongoose.model('Memo');
 
 const NOT_FOUND = { message: 'Not found', error_code: 404 };
 
+const unwrap = ({ title, content, tags, summary }) => ({
+    title,
+    content,
+    tags,
+    summary
+  });
+
 exports.all = async function(req, res) {
   const memos = await Memo.find({ user_id: req.user.user_id });
   res.send(memos);
@@ -13,11 +20,16 @@ exports.create = async function(req, res) {
   const memo = new Memo({
     uuid: uuid(),
     user_id: req.user.user_id,
-    created_time: Date.now()
+    created_time: Date.now(),
+    title: req.body.title,
+    content: req.body.content,
+    summary: req.body.summary,
+    tags: req.body.tags
   });
-  memo.save();
+
   res.status(201);
   res.set({ Location: `/memos/${memo.uuid}` });
+  res.send(memo)
   res.end();
 };
 
@@ -35,12 +47,6 @@ exports.get = async function(req, res) {
 };
 
 exports.update = async function(req, res) {
-  const unwrap = ({ title, content, tags, summary }) => ({
-    title,
-    content,
-    tags,
-    summary
-  });
   const memo = await Memo.findOneAndUpdate(
     { uuid: req.params.uuid, user_id: req.user.user_id },
     { ...unwrap(req.body), updated_time: Date.now() },
