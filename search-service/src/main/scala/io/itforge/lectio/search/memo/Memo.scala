660936a95c14720c9fdd6bfb030bc7e25b55eebb
kavinvin <kavinvin.vin@gmail.com>
2019-05-04 18:45:36 +0700

    Remove created_time field in Memo

--- search-service/src/main/scala/io/itforge/lectio/search/memo/Memo.scala
+++ search-service/src/main/scala/io/itforge/lectio/search/memo/Memo.scala
@@ -1,11 +1,5 @@
 package io.itforge.lectio.search.memo
 
-import java.util.{Date, UUID}
+import java.util.UUID
 
-case class Memo(
-    uuid: UUID,
-    title: String,
-    content: String,
-    tags: Set[String],
-    user_id: UUID,
-    created_time: Date)
+case class Memo(uuid: UUID, title: String, content: String, tags: Set[String], user_id: UUID)
